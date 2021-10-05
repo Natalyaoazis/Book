b8fd1e8510a32e7f8cd0a8d68548f44262156fce
Natalya Mikhaylova <natalyaoazis82@gmail.com>
2021-10-05 08:19:03 +0300

    test find added

--- src/test/java/ru/netology/repository/ProductRepositoryTest.java
+++ src/test/java/ru/netology/repository/ProductRepositoryTest.java
@@ -1,5 +1,6 @@
 package ru.netology.repository;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.netology.domain.Book;
 import ru.netology.domain.Product;
@@ -9,22 +10,23 @@ import ru.netology.domain.Smartphone;
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProductRepositoryTest {
+    ProductRepository repository = new ProductRepository();
+    Product book1 = new Book(1, "book1", 500,"автор1");
+    Product book2 = new Book(2, "book2", 600,"автор2");
+    Product smartphone1 = new Smartphone(11, "iPhone", 25000, "Apple");
+    Product smartphone2 = new Smartphone(22, "iPhone2", 50000, "Apple");
 
-
-    @Test
-    public void shouldSaveAll() {
-        ProductRepository repository = new ProductRepository();
-        Product book1 = new Book(1, "book1", 500,"автор1");
-        Product book2 = new Book(2, "book2", 600,"автор2");
-        Product smartphone1 = new Smartphone(11, "iPhone", 25000, "Apple");
-        Product smartphone2 = new Smartphone(22, "iPhone2", 50000, "Apple");
-
-
+    @BeforeEach
+    public void setUp() {
         repository.save(book1);
         repository.save(book2);
         repository.save(smartphone1);
         repository.save(smartphone2);
+    }
 
+
+    @Test
+    public void shouldSaveAll() {
         Product[] expected = {book1, book2, smartphone1, smartphone2};
         Product[] actual = repository.findAll();
         assertArrayEquals(expected, actual);
@@ -32,20 +34,17 @@ class ProductRepositoryTest {
 
     @Test
     public void shouldRemoveById() {
-        ProductRepository repository = new ProductRepository();
-        Product book1 = new Book(1, "book1", 500,"автор1");
-        Product book2 = new Book(2, "book2", 600,"автор2");
-        Product smartphone1 = new Smartphone(11, "iPhone", 25000, "Apple");
-        Product smartphone2 = new Smartphone(22, "iPhone2", 50000, "Apple");
-
-        repository.save(book1);
-        repository.save(book2);
-        repository.save(smartphone1);
-        repository.save(smartphone2);
-
         repository.removeById(11);
         Product[] expected = {book1, book2, smartphone2};
         Product[] actual = repository.findAll();
         assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldFindAll() {
+        repository.findAll();
+        Product[] expected = {book1, book2, smartphone1, smartphone2};
+        Product[] actual = repository.findAll();
+        assertArrayEquals(expected,actual);
     }
 }
